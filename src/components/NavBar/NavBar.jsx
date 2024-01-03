9659145724ef60ffd14ae84d124f531259c128db
Colten Bowdoin <coltenbowdoin@gmail.com>
2024-01-03 12:52:54 -0600

    Completed all functionality of the app.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,22 +5,14 @@ import AuthContext from "../../context/AuthContext";
 import "./NavBar.css";
 
 const Navbar = () => {
-  const { logoutUser, user } = useContext(AuthContext);
   const navigate = useNavigate();
   return (
     <div className="navBar">
       <ul>
         <li className="brand">
           <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            <b>React/ASP.NET JWT Starter</b>
+            <b>Your Local Weather</b>
           </Link>
-        </li>
-        <li>
-          {user ? (
-            <button onClick={logoutUser}>Logout</button>
-          ) : (
-            <button onClick={() => navigate("/login")}>Login</button>
-          )}
         </li>
       </ul>
     </div>
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -25,11 +25,44 @@ const HomePage = () => {
     event.preventDefault();
     if (zipCode) {
       displayWeatherDetails(zipCode);
+      setZipCode("")
     }
   }
-  return(
+
+  function clearData() {
+    setWeatherDetails(null);
+  }
+
+  function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  return (
     <div>
-      
+      <form onSubmit={handleSubmit}>
+        Enter Zip Code for Local Weather: 
+        <input type="number" min="1" max="99999" onChange={(event) => setZipCode(event.target.value)}/>
+        <button type="submit">Get Weather</button>
+      </form>
+      {weatherDetails && (
+        <div>
+          <h2>Weather Where You Live</h2>
+          <p>Location: {weatherDetails.name}</p>
+          <p>Temperature: {weatherDetails.main.temp}</p>
+          <p>Feels Like: {weatherDetails.main.feels_like}</p>
+          <p>Humidity: {weatherDetails.main.humidity}</p>
+          <p>High for the day: {weatherDetails.main.temp_max}</p>
+          <p>Low for the day: {weatherDetails.main.temp_min}</p>
+          <p>Wind Speed: {weatherDetails.wind.speed}</p>
+          <p>Wind Degree: {weatherDetails.wind.deg}</p>
+          <h3>Coordinates</h3>
+          <p>Latitude:{weatherDetails.coord.lat}</p>
+          <p>Longitude: {weatherDetails.coord.lon}</p>
+          <h3>{capitalizeFirstLetter(weatherDetails.weather[0].description)}</h3>
+        </div>
+      )};
+        {weatherDetails && (
+        <button onClick={clearData}>Reset</button>)}
     </div>
   )
 };
